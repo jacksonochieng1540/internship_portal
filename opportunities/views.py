69da405c06d3671454efd4af63b9dad0d78c9805
Jack ayaga <jacksonochieng1540@gmail.com>
2025-11-14 22:15:06 +0300

    Update views.py

--- opportunities/views.py
+++ opportunities/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.conf import settings  # Import settings
+from django.conf import settings  
 from .models import Opportunity, Application, Company
 from .forms import OpportunityForm, ApplicationForm, CompanyForm
 from .filters import OpportunityFilter
